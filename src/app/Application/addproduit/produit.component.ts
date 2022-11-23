1a9b1a5c9b0e14be777f34c51ed9a9e523c1a4c7
yassine ben ammar <yassinebenammar145@gmail.com>
2022-11-23 11:46:52 +0000

    Partie 1 Application TP9 finie

--- src/app/Application/addproduit/produit.component.ts
+++ src/app/Application/addproduit/produit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-produit',
@@ -10,13 +10,26 @@ export class ProduitComponent implements OnInit {
   
   productForm!:FormGroup;
   constructor(private fb:FormBuilder) { }
-
+  public get idProduct()
+  { return this.productForm.get('id'); }
+  isValidPattern(){
+    return this.productForm.controls['libelle'].errors?.['pattern'] &&
+    this.productForm.controls['libelle'].dirty;
+    }
+    public get libelle(){
+      return this.productForm.get('libelle');
+    }
   ngOnInit(): void {
     this.productForm = this.fb.nonNullable.group({
       id:[0],
       libelle:[''],
       couleurs: this.fb.array([])
     })
+    this.productForm = this.fb.nonNullable.group({
+      id:[0, Validators.required],
+      libelle:['', [Validators.required, Validators.pattern('^[A-Z][a-zA-Z]+$')]]
+      })
+      
   }
 
 }
